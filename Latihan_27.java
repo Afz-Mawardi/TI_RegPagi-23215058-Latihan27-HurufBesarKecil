a9337b5259dcbbf3e7676310834ee99ad429a58d
Afz-Mawardi <fitrinarti341@gmail.com>
2024-11-02 21:44:37 +0700

    Add files via upload

---
+++ Latihan_27.java
@@ -0,0 +1,28 @@
+package Tugas_Latihan;
+/**
+ *
+ * @author user
+ * NAMA     : Afsani Wahyu Mawardi
+ * KELAS    : TI RegPagi
+ * NIM      : 23215058
+ * Dekskipsi 
+ * Progam   : Progam ini untuk menampilkan Formating Huruf
+ *          Huruf Besar Kecil
+ */
+import java.util.Scanner;
+
+public class Latihan_27 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan Kalimat: ");
+        String kalimat = scanner.nextLine();
+
+        System.out.println("\n====Hasil Formating====");
+        System.out.println("Huruf Besar : " + kalimat.toUpperCase());
+        System.out.println("Huruf Kecil : " + kalimat.toLowerCase());
+
+        scanner.close();
+    }
+}
